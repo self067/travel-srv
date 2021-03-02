5c0092ad51e85795e85cd9d50899a5f20a63e88b
self067 <seltor@mail.ru>
2021-03-02 23:01:50 +0300

    init: start travel srv

---
+++ src/dao/commentsDAO.js
@@ -0,0 +1,119 @@
+import { ObjectId } from "bson"
+
+let comments
+
+export default class CommentsDAO {
+  static async injectDB(conn) {
+    if (comments) {
+      return
+    }
+    try {
+      comments = await conn.db(process.env.MFLIX_NS).collection("comments")
+    } catch (e) {
+      console.error(`Unable to establish collection handles in userDAO: ${e}`)
+    }
+  }
+
+/**
+   * @param {string} movieId - The _id of the movie in the `movies` collection.
+   * @param {Object} user - An object containing the user's name and email.
+   * @param {string} comment - The text of the comment.
+   * @param {string} date - The date on which the comment was posted.
+   * @returns {DAOResponse} Returns an object with either DB response or "error"
+   */
+  static async addComment(movieId, user, comment, date) {
+    try {
+      const commentDoc = { movie_id: ObjectId(movieId), name: user.name, email: user.email,
+	text: comment, date };
+
+      return await comments.insertOne(commentDoc)
+    } catch (e) {
+      console.error(`Unable to post comment: ${e}`)
+      return { error: e }
+    }
+  }
+
+  /**
+   * Updates the comment in the comment collection. Queries for the comment
+   * based by both comment _id field as well as the email field to doubly ensure
+   * the user has permission to edit this comment.
+   * @param {string} commentId - The _id of the comment to update.
+   * @param {string} userEmail - The email of the user who owns the comment.
+   * @param {string} text - The updated text of the comment.
+   * @param {string} date - The date on which the comment was updated.
+   * @returns {DAOResponse} Returns an object with either DB response or "error"
+   */
+  static async updateComment(commentId, userEmail, text, date) {
+    try {
+      const updateResponse = await comments.updateOne(
+        { "_id": commentId, email: userEmail },
+        { $set: { text, date } },
+      )
+
+      return updateResponse
+    } catch (e) {
+      console.error(`Unable to update comment: ${e}`)
+      return { error: e }
+    }
+  }
+
+  static async deleteComment(commentId, userEmail) {
+
+    try {
+      const deleteResponse = await comments.deleteOne({
+        _id: ObjectId(commentId), email: userEmail 
+      })
+
+      return deleteResponse
+    } catch (e) {
+      console.error(`Unable to delete comment: ${e}`)
+      return { error: e }
+    }
+  }
+
+  static async mostActiveCommenters() {
+    /**
+    Ticket: User Report
+    Build a pipeline that returns the 20 most frequent commenters on the 
+    site. You can do this by counting the number of occurrences of a user's
+    email in the `comments` collection.
+    */
+    try {
+      // Return the 20 users who have commented the most 
+      const pipeline =  
+	[
+	  {
+	    '$project': {
+	      'email': 1, 
+	      '_id': 0
+	    }
+	  }, {
+	    '$group': {
+	      '_id': '$email', 
+	      'count': {
+	        '$sum': 1
+	      }
+	    }
+	  },{ $sort: { count: -1} },
+		{$limit: 20}
+	];
+
+      const readConcern = { level: "majority" }; //comments.readConcern
+      const aggregateResult = await comments.aggregate(pipeline, {
+        readConcern,
+      })
+
+      return await aggregateResult.toArray()
+    } catch (e) {
+      console.error(`Unable to retrieve most active commenters: ${e}`)
+      return { error: e }
+    }
+  }
+}
+
+/**
+ * Success/Error return object
+ * @typedef DAOResponse
+ * @property {boolean} [success] - Success
+ * @property {string} [error] - Error
+ */
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import app from './server';
+import { MongoClient } from 'mongodb';
+import MoviesDAO from '../src/dao/moviesDAO';
+import UsersDAO from './dao/usersDAO';
+import CommentsDAO from './dao/commentsDAO';
+
+const port = process.env.PORT || 8000;
+const poolSize = 100;
+
+MongoClient.connect(process.env.MFLIX_DB_URI, {
+  poolSize,
+  useNewUrlParser: true,
+  writeConcern: { wtimeout: 3500 },
+  useUnifiedTopology: true,
+})
+  .catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
+  })
+  .then(async (client) => {
+    await MoviesDAO.injectDB(client);
+    await UsersDAO.injectDB(client);
+    await CommentsDAO.injectDB(client);
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`);
+    });
+  });
