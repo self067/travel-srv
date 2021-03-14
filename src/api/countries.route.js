716a330f633af015bcd8a5efa12607e38aaf0755
self067 <seltor@mail.ru>
2021-03-14 14:38:19 +0300

    feat: change countries? add some fields

--- src/api/countries.route.js
+++ src/api/countries.route.js
@@ -4,19 +4,18 @@ import CommentsCtrl from './comments.controller';
 
 const router = new Router();
 
-// associate put, delete, and get(id)
 router.route('/').get(CountriesCtrl.apiGetCountries);
 router.route('/search').get(CountriesCtrl.apiSearchCountries);
 router.route('/params').get(CountriesCtrl.apiGetCountriesByParams);
-router.route('/facet-search').get(CountriesCtrl.apiFacetedSearch);
+router.route('/live-search').get(CountriesCtrl.apiLiveSearch);
 router.route('/id/:id').get(CountriesCtrl.apiGetCountryById);
 
 router.route('/sights/:sightId').get(CommentsCtrl.apiGetCommentsBySightId);
 
-// router
-//   .route('/comment')
-//   .post(CommentsCtrl.apiPostComment)
-//   .put(CommentsCtrl.apiUpdateComment)
-//   .delete(CommentsCtrl.apiDeleteComment);
+router
+  .route('/comment')
+  .post(CommentsCtrl.apiPostComment)
+  .put(CommentsCtrl.apiUpdateComment)
+  .delete(CommentsCtrl.apiDeleteComment);
 
 export default router;
