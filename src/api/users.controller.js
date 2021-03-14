300f0c175dabc8c2377c918ec269718d2394559e
self067 <seltor@mail.ru>
2021-03-14 14:38:51 +0300

    feat: add users avatar

--- src/api/users.controller.js
+++ src/api/users.controller.js
@@ -5,16 +5,18 @@ import UsersDAO from '../dao/usersDAO';
 const hashPassword = async (password) => await bcrypt.hash(password, 10);
 
 export class User {
-  constructor({ name, email, password, preferences = {} } = {}) {
+  constructor({ name, email, password, avatar = '', preferences = {} } = {}) {
     this.name = name;
     this.email = email;
     this.password = password;
+    this.avatar = avatar;
     this.preferences = preferences;
   }
   toJson() {
     return {
       name: this.name,
       email: this.email,
+      avatar: this.avatar,
       preferences: this.preferences,
     };
   }
@@ -148,39 +150,6 @@ export default class UserController {
     }
   }
 
-  static async delete(req, res) {
-    try {
-      let { password } = req.body;
-      if (!password || typeof password !== 'string') {
-        res
-          .status(400)
-          .json({ error: 'Bad password format, expected string.' });
-        return;
-      }
-      const userJwt = req.get('Authorization').slice('Bearer '.length);
-      const userClaim = await User.decoded(userJwt);
-      var { error } = userClaim;
-      if (error) {
-        res.status(401).json({ error });
-        return;
-      }
-      const user = new User(await UsersDAO.getUser(userClaim.email));
-      if (!(await user.comparePassword(password))) {
-        res.status(401).json({ error: 'Make sure your password is correct.' });
-        return;
-      }
-      const deleteResult = await UsersDAO.deleteUser(userClaim.email);
-      var { error } = deleteResult;
-      if (error) {
-        res.status(500).json({ error });
-        return;
-      }
-      res.json(deleteResult);
-    } catch (e) {
-      res.status(500).json(e);
-    }
-  }
-
   static async save(req, res) {
     try {
       const userJwt = req.get('Authorization').slice('Bearer '.length);
@@ -201,63 +170,6 @@ export default class UserController {
       res.json({
         auth_token: updatedUser.encoded(),
         info: updatedUser.toJson(),
-      });
-    } catch (e) {
-      res.status(500).json(e);
-    }
-  }
-
-  // for internal use only
-  static async createAdminUser(req, res) {
-    try {
-      const userFromBody = req.body;
-      let errors = {};
-      if (userFromBody && userFromBody.password.length < 8) {
-        errors.password = 'Your password must be at least 8 characters.';
-      }
-      if (userFromBody && userFromBody.name.length < 3) {
-        errors.name = 'You must specify a name of at least 3 characters.';
-      }
-
-      if (Object.keys(errors).length > 0) {
-        res.status(400).json(errors);
-        return;
-      }
-
-      const userInfo = {
-        ...userFromBody,
-        password: await hashPassword(userFromBody.password),
-      };
-
-      const insertResult = await UsersDAO.addUser(userInfo);
-      if (!insertResult.success) {
-        errors.email = insertResult.error;
-      }
-
-      if (Object.keys(errors).length > 0) {
-        res.status(400).json(errors);
-        return;
-      }
-
-      const makeAdminResponse = await UsersDAO.makeAdmin(userFromBody.email);
-
-      const userFromDB = await UsersDAO.getUser(userFromBody.email);
-      if (!userFromDB) {
-        errors.general = 'Internal error, please try again later';
-      }
-
-      if (Object.keys(errors).length > 0) {
-        res.status(400).json(errors);
-        return;
-      }
-
-      const user = new User(userFromDB);
-      const jwt = user.encoded();
-      const loginResponse = await UsersDAO.loginUser(user.email, jwt);
-
-      res.json({
-        auth_token: jwt,
-        info: user.toJson(),
       });
     } catch (e) {
       res.status(500).json(e);
